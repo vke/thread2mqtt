e97d0aca66c1d65b2bdafb2965c7f213cf1e79bf
kolbert <kolbert@yandex.ru>
2020-09-16 21:55:31 +0300

    add index file for loading convertors

---
+++ convertors/index.js
@@ -0,0 +1,9 @@
+const converters = {};
+require('fs').readdirSync(__dirname)
+    .filter(fn => fn !== 'index.js')
+    .map(str => str.substr(0, str.length - 3))
+    .forEach(name => {
+        converters[name] = require(`./${name}`)
+    })
+
+module.exports = converters
